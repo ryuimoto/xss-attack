71d50e885682baf0f55213784e5e2390c2d9270a
maca <narrow617@gmail.com>
2022-04-25 18:25:39 +0900

    add dummydata

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+use App\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +14,18 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for ($i=0; $i < 1000; $i++) { 
+            dump("num".$i);    
+        
+            $a = str_random(8);
+            $b = str_random(8);
+            $c = str_random(8);
+            
+            User::create([
+                'a' => $a,
+                'b' => $b,
+                'c' => $c,
+            ]);
+        }
     }
 }
